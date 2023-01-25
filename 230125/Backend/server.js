6d26aec259f8df3caaecfad96711d5fd8e9f553d
Hynn <choihwoong@me.com>
2023-01-25 10:33:59 +0900

    feat : initilization setting file-upload

---
+++ 230125/Backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const cors = require('cors');
+
+app.set("view engine", "html");
+nunjucks.configure("views", {express: app,});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+app.get("/single", (req,res) => {
+    res.render('single.html')
+})
+
+app.get("/array", (req,res) => {
+    res.render('array.html')
+})
+
+app.get("/upload", (req,res) => {
+    res.render('upload.html')
+})
+
+
+app.listen(3000,()=>{
+    console.log('listening on http://localhost/3000');
+})
